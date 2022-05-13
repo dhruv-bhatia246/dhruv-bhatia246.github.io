a4510bea3df6d558b2db573a247cc48d2719e95e
dhruv.bhatia246 <dhruv.bhatia246@gmail.com>
2022-05-13 16:43:54 +0530

    Updates

---
+++ precache-manifest.840e73c0f2a5747f204235d0eeb95c1a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "593d97c14583eb96f0b439dc467aff90",
+    "url": "/index.html"
+  },
+  {
+    "revision": "32b7f2151fa3c21b4378",
+    "url": "/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "d719958679f70611c763",
+    "url": "/static/css/main.1684c906.chunk.css"
+  },
+  {
+    "revision": "32b7f2151fa3c21b4378",
+    "url": "/static/js/2.5c60a38e.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/static/js/2.5c60a38e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d719958679f70611c763",
+    "url": "/static/js/main.ec8a694e.chunk.js"
+  },
+  {
+    "revision": "a2d41c56873c741b51b2",
+    "url": "/static/js/runtime-main.a5042f6b.js"
+  }
+]);
